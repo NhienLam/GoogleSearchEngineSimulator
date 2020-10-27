271af0e15b67144da3568e4f47f29786b8522f3d
Nhien (Ricky) Lam <62775270+NhienLam@users.noreply.github.com>
2020-10-27 10:29:50 -0700

    Update SearchEngineRunner.java

--- SearchEngineRunner.java
+++ SearchEngineRunner.java
@@ -7,6 +7,7 @@ package searchengine;
  */
 public class SearchEngineRunner 
 {
+	// Creates and runs the SearchEngine
 	public static void main(String[] args)
 	{
 		SearchEngine se = new SearchEngine();
